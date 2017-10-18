dc3c9049251a5596dbd189ad75524e7368b40ffb
David S. Jackson <deepbsd@yahoo.com>
2017-10-18 16:55:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Binary and Bitwise Operators
+
+From Thinkful's computer science curriculum, these exercises
+apply some principles of binary logic and bitwise operators.
---
+++ twosCompliment.js
@@ -0,0 +1,10 @@
+var toBinary = function toBinary(num){
+	num = parseInt(num);
+	return (-num >>> 0).toString(2);
+}
+
+var numbers = [23, 14, -18, 91, 20]
+
+numbers.forEach(function(num){
+	console.log('Number: ',num,'Binary: ',toBinary(num))
+})
